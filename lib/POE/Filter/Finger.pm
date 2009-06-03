70c7a0c650fd1eda88cec5e4b4f901b54fed55d0
Chris Williams <chris@bingosnet.co.uk>
2009-06-03 08:53:44 +0000

    Import of BINGOS/POE-Filter-Finger-0.08 from CPAN.
    gitpan-cpan-distribution: POE-Filter-Finger
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         BINGOS/POE-Filter-Finger-0.08.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/POE/Filter/Finger.pm
+++ lib/POE/Filter/Finger.pm
@@ -5,7 +5,7 @@ use warnings;
 use vars qw($VERSION);
 use base qw(POE::Filter);
 
-$VERSION = '0.06';
+$VERSION = '0.08';
 
 #  {Q1}    ::= [{W}|{W}{S}{U}]{C}
 #  {Q2}    ::= [{W}{S}][{U}]{H}{C}
@@ -20,7 +20,7 @@ my $h_regex = qr{[-_a-z0-9.]+}i;
 
 sub new {
   my $class = shift;
-  my $self = {};
+  my $self = {@_};
   $self->{BUFFER} = [];
   bless $self, $class;
 }
@@ -261,7 +261,7 @@ Ricardo SIGNES <rjbs@cpan.org>
 
 =head1 LICENSE
 
-Copyright C<(c)> Chris Williams and Ricardo SIGNES
+Copyright E<copy> Chris Williams and Ricardo SIGNES
 
 This module may be used, modified, and distributed under the same terms as Perl itself. Please see the license that came with your Perl distribution for details.
 
